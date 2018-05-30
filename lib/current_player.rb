e45235959621761830a73551ae37ede9347ed276
Sharmaine Requerme <srqrme@gmail.com>
2018-05-30 16:58:53 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,10 @@
 def turn_count(board)
   turn_counter = 0
-  board.each{|character| character == "X" || character == "O" ? turn_counter += 1 : else}
+  board.each do |character|
+    if character == "X" || character == "O"
+      turn_counter += 1
+    end
+  end
   turn_counter
 end
 
